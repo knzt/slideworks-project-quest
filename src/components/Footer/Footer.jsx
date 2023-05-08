5b6f3101d3740da64b754e5b48300840e04d147d
knzt <cristinahellen46@gmail.com>
2023-05-07 22:39:20 -0300

    pequenos ajustes nos estilos

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,25 +10,30 @@ function Footer() {
       <img src={Logo} alt="Logo" />
       <hr />
       <div className="footer__info">
-        <small>Terms & Conditions</small>
-        <small>Privacy Policy</small>
+        <small>
+            <a href="#" title="Acessar página de termos e condições">Terms & Conditions</a>
+        </small>
+        <small><a href="#" title="Acessar página de políticas de privacidade">Privacy Policy</a></small>
       </div>
       <div className="footer__icons footer__social-icons">
           <a
           href="https://www.facebook.com/"
-          target="blank">
+          target="blank"
+          title="Acesse nossa página no Facebook">
             <FaFacebook />
           </a>
 
           <a
           href="https://twitter.com/"
-          target="blank">
+          target="blank"
+          title="Acesse nosso perfil no Twitter">
             <FaTwitter />
           </a>
 
           <a
           href="https://www.instagram.com/"
-          target="blank">
+          target="blank"
+          title="Acesse nosso perfil no Instagram">
             <FaInstagram />
           </a>
         </div>
